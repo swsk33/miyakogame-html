02cd265f5023ffcf8391a115601abbf655414be1
swsk33 <yanhanhui2001a@163.com>
2020-09-17 15:42:40 +0800

    v1.0.0-开始做布丁事件

--- js/pc/character-control.js
+++ js/pc/character-control.js
@@ -1,9 +1,9 @@
-//宫子的控制
+//宫子的行为模块
 let miyako = document.querySelector('.game .gameBg .miyako');
 let y = 0;
 miyako.style.top = y + 'px';
 
-//按下↑或者w键且在界内时，宫子向上移动
+//宫子向上移动
 function up(i) {
 	if (y > i) {
 		y = y - i;
@@ -13,7 +13,7 @@ function up(i) {
 	}
 }
 
-//按下↓或者s键且在界内时，宫子向下移动
+//宫子向下移动
 function down(i) {
 	if ((y + miyako.offsetHeight) < bgHeight - i) {
 		y = y + i;
