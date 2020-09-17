4b8cec07e2983ad28958ada290ad0f13f9b6b8e8
swsk33 <yanhanhui2001a@163.com>
2020-09-17 16:24:56 +0800

    v1.0.0-正在设计布丁模型

--- js/pc/pudding-control.js
+++
@@ -1,7 +0,0 @@
-//控制布丁的移动
-let puddingArray = [];//存放布丁对象的数组
-let puds = document.querySelector('.gameBg .puddings').children;
-function getPuddings() {
-	
-	
-}
---
+++ js/pc/pudding.js
@@ -0,0 +1,28 @@
+//控制布丁的移动
+let puddingArray = []; //存放布丁对象的数组
+let puds = document.querySelector('.gameBg .puddings').children;
+//获取布丁
+function getPuddings() {
+	for (let i = 0; i < puds.length; i++) {
+		let col = puds[i].children;
+		//分数判定与设置
+		let scoreEach;
+		if (i == 2) {
+			scoreEach = 2;
+		} else if (i == 3) {
+			scoreEach = 3;
+		} else {
+			scoreEach = 1;
+		}
+		for (let j = 0; j < col; j++) {
+			//读取并构建每一个布丁对象并存入全局数组
+			let eachPudding = {
+				dom: col[j],
+				isEaten: false,
+				score: scoreEach
+			};
+			puddingArray.push(eachPudding);
+		}
+	}
+	console.log(puddingArray);
+}
