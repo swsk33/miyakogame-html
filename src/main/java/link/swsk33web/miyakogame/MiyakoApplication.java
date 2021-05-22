f034c89f0a9e848880424e336e92abcad4f53ded
swsk33 <yanhanhui2001a@163.com>
2021-05-22 12:17:48 +0800

    v3.0.2

--- src/main/java/link/swsk33web/miyakogame/MiyakoApplication.java
+++ src/main/java/link/swsk33web/miyakogame/MiyakoApplication.java
@@ -1,7 +1,14 @@
 package link.swsk33web.miyakogame;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
@@ -10,6 +17,35 @@ public class MiyakoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiyakoApplication.class, args);
+	}
+
+	/**
+	 * http自动跳转https
+	 */
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8801); // 原http端口
+		connector.setSecure(false);
+		connector.setRedirectPort(8443); // 跳转的https端口，也是我们配置的项目端口
+		return connector;
 	}
 
 }
